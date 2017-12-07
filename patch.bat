381553fa93481920d1e5869aa1a5e7e59308ce17
Ivan Bereziuk <Navi.Bereziuk@gmail.com>
2017-12-07 21:54:11 +0100

    add script to update cmd ls, prompt + initial work dir

---
+++ patch.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+echo [35mPatching cmd ...
+chcp 1252
+
+:: set the prompt string
+PROMPT $E[32mJohn $E[97m$P$G$S$E[0m
+
+DOSKEY ls=(ECHO ^| set /p=[36m) ^& (DIR /B /A:D $*) ^& (ECHO ^| set /p=[33m) ^& (DIR /B /A:-D $*) ^& (ECHO ^| set/p=[0m)
+:: change starting directory 
+cd /D D:\workdir
+
+:: set default color back
+echo [0m
