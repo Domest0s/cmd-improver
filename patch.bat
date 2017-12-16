586f101c1ce1f738c920337e9c06ff98a03a17ea
Ivan Bereziuk <Navi.Bereziuk@gmail.com>
2017-12-16 16:17:55 +0100

    add pwd command

--- patch.bat
+++ patch.bat
@@ -6,7 +6,9 @@ chcp 1252
 :: set the prompt string
 PROMPT $E[32mJohn $E[97m$P$G$S$E[0m
 
+:: convenient commands:
 DOSKEY ls=(ECHO ^| set /p=[36m) ^& (DIR /B /A:D $*) ^& (ECHO ^| set /p=[33m) ^& (DIR /B /A:-D $*) ^& (ECHO ^| set/p=[0m)
+DOSKEY pwd=ECHO %%CD%%
 :: change starting directory 
 cd /D D:\workdir
 
